a46f56ede3e259f22def4b2a9d06cc0a7e2b8cf1
Rizal Fadlullah <izzal@Rizals-MacBook-Air.local>
2025-05-20 17:27:28 +0700

    publish powershell script

---
+++ scripts/install.ps1
@@ -0,0 +1,27 @@
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+# Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+scoop install git
+scoop bucket add extras
+scoop bucket add nerd-fonts
+
+scoop install alacritty
+scoop install neovim
+scoop install gh
+scoop install JetBrainsMono-NF
+scoop install uutils-coreutils
+scoop install vcredist2022
+scoop install yazi
+scoop install openssh
+
+New-Item "$env:APPDATA\alacritty\alacritty.toml" -ItemType File -Force
+
+@"
+[font]
+normal = { family = "JetBrainsMono Nerd Font", style = "Regular" }
+"@ | Add-Content -Path "$env:APPDATA\alacritty\alacritty.toml"
+
+# git clone https://github.com/izzalDev/NvChad.git $ENV:USERPROFILE\AppData\Local\nvim
+# nvim --headless "+Lazy! sync" +qa
+
+
+
