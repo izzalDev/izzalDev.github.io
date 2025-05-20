b645bd8a4bb30290bb4c673aa6c7050a882d4d87
Rizal Fadlullah <izzal@Rizals-MacBook-Air.local>
2025-05-20 16:36:59 +0700

    chore: update devcontainer setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Setting up environment..."
 
-apt-get update
-apt-get install -y curl git openssh-server
-
+apt update
+apt install -y curl git openssh-server gh neovim yazi
+curl -sS https://starship.rs/install.sh | sh
+git clone https://github.com/izzalDev/NvChad.git ~/.config/nvim
+nvim --headless "+Lazy! sync" +qa
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
 echo "Environment setup complete"
